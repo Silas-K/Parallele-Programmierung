a275e27f90b8817454a46065805c51020ae15a1d
Silas Köhnke <silaskoehnke@gmx.de>
2024-04-10 15:03:31 +0200

    comments

--- 01/pthread_mutex.c
+++ 01/pthread_mutex.c
@@ -11,13 +11,15 @@ void* print_message_thread(void* ptr)
     char* message;
     message = (char*) ptr;
     printf("%s \n", message);
-    
+
+    // Start locking for this critical section    
     pthread_mutex_lock(&mutex1);
     for (int i = 0; i < 100000; i++) 
     {
         counter ++;
     }
-
+    
+    // Release the lock, critical section ended
     pthread_mutex_unlock(&mutex1);
 }
 
@@ -29,14 +31,19 @@ int main()
 
     int iret1, iret2;
 
+    // create 2 threads 
     iret1 = pthread_create(&thread1, NULL, print_message_thread, (void*) message1);
     iret2 = pthread_create(&thread2, NULL, print_message_thread, (void*) message2);
 
+    // wait for all threads to finish
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
     printf("Thread 1 returns: %d\n", iret1);
     printf("Thread 2 returns: %d\n", iret2);
+
+    // -> expected result:
+    // counter = 200.000
     printf("Result counter value : %d\n", counter);
 
     exit(0);
